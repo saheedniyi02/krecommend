9f1612e154e5ad9d9e7de0a0de3589d14719a57d
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-05-26 03:24:34 +0100

    Add files via upload

--- recommend.py
+++ recommend.py
@@ -14,30 +14,37 @@ class KRecommend:
         self.len_vocabulary=0
 
     def fit(self, data, text_columns):
+        #checks if the text_columns is a list
         assert isinstance(text_columns,list),"'text_columns' parameter must be a list"
         self.text_columns = text_columns
 
-        # handles 1 column or multiple columns
+        # for 1 text_column 
         if len(self.text_columns) == 1:
+            #checks if column exists,creates the train data and raises an exception if a column not of type "string" was passed
             try:
-                #checks if column exists,creates the train data and raises an exception if a column not of type "string" was passed
                 self.train = data[self.text_columns[0]]
             except KeyError:
                 raise Exception("Column cannot be found in the dataframe")
             if ( data[self.text_columns[0]].dtype!="object"):
                 raise Exception(f"{self.text_columns[0]} column isn't' a string column, pass a column of type string.")
+                
+        #for multiple columns
         elif len(self.text_columns) > 1:
             self.train = ""
             for column in self.text_columns:
                 if column not in data.columns:
                 	raise Exception(f"{column} column cannot be found in dataframe")
-                #checks if column exists,creates the train data and gives a warning if one of the columns were of type "int".
+                #checks if column exists,creates the train data and gives a warning if one of the columns is not of type "string".
                 if ( data[column].dtype!="object"):
                 	data[column]=data[column].astype("object")
                 	warnings.warn(f"{column} column isn't' a string column, you might want to change it to type string")
                 self.train += self.train + data[column]+ " "
+                
+        #fill missing values
         self.train.fillna("",inplace=True)
+        #get index
         self.train_index = self.train.index
+        #fit the vectorizer on the train set and transform the train test.
         self.__vectorized_train = self.__vectorizer.fit_transform(self.train)
 
         # records if the model has been trained or not and identifies which type of training
@@ -47,6 +54,7 @@ class KRecommend:
         print(f"KRecommender fitted successfully with {self.len_vocabulary} words in the vocabulary!")
 
     def fit_on_sql_table(self, table_name, id_column, text_columns, connection):
+        #checks if the text_columns is a list
         assert isinstance(text_columns,list),"'text_columns'' parameter must be a list"
         self.text_columns = text_columns
         self.id_column = id_column
@@ -61,7 +69,7 @@ class KRecommend:
         	data.set_index(self.id_column, inplace=True)
         except KeyError:
         	raise Exception(f"{self.id_column} column cannot be found in the {self.table_name} table")     
-        # handles 1 column or multiple columns
+        # for 1 column 
         if len(self.text_columns) == 1:
             try:
             	self.train = data[self.text_columns[0]]
@@ -69,6 +77,8 @@ class KRecommend:
             	raise Exception(f"Column cannot be found in {self.table_name} table")
             if ( data[self.text_columns[0]].dtype!="object"):
                 raise Exception(f"{self.text_columns[0]} column isn't' a string column, pass a column of type string.")
+         
+        #for multiple columns
         elif len(self.text_columns) > 1:
             self.train = ""
             for column in self.text_columns:
@@ -76,6 +86,7 @@ class KRecommend:
                 	raise Exception(f"{column} column cannot be found in {self.table_name} table") 	
                 if ( data[column].dtype!="object"):
                 	data[column]=data[column].astype("object")
+                	print(data[column].dtype)
                 	warnings.warn(f"{column} column isn't' a string column, you might want to change it to type string")
                 self.train += self.train + data[column]+ " "
         self.train.fillna("",inplace=True) 
@@ -89,6 +100,8 @@ class KRecommend:
         print(f"KRecommender fitted successfully with {self.len_vocabulary} words in the vocabulary!")
 
     def predict(self, test):
+        
+        #asserts that input is a list and has same length as train data
         assert isinstance(test,list),"'test'' parameter must be a list"
         assert len(test)==len(self.text_columns),f"KRecommend was trained on {len(self.text_columns)} columns while your input has {len(test)} columns"
         if self.__is_trained == False:
@@ -141,20 +154,3 @@ class KRecommend:
         else:
         	recommendations.pop(min_id)
         return recommendations
-
-
-"""
-1)Error if an in is put into it.Done
-2)Error if there are missing values.Done
-3)Error if it hasnt been trained.Done
-4)Error if it wants to train on another type.Done
-5)Error if there is no table.Done
-6)Error if text column or id column can't be found.Done
-7)Ensure the inputs are lists.Done
-8)Print number of words in vocabulary.Done
-9)Remove first one if it is 1.Done
-future
-1)additional cleaning
-2)filter
-3)Preference
-"""
